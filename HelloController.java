2c4a423f86403ffa3f754f2e1c5c9e2cf41cc94b
reddy <78945979+Reddytirumala@users.noreply.github.com>
2026-06-26 16:54:42 +0530

    Create HelloController.java

---
+++ HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/")
+
+    public String home() {
+
+        return "Hello from Spring Boot running in Kubernetes";
+
+    }
+
+    @GetMapping("/health")
+
+    public String health() {
+
+        return "Application is Healthy";
+
+    }
+
+}
